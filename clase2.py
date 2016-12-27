916e9daf13d05e59d6131b339d21c745aff9972e
dilinayo <diego.x.linayo.-nd@disney.com>
2016-12-27 11:05:00 -0300

    Refactorizacion de codigo

--- clase2.py
+++ clase2.py
@@ -6,10 +6,6 @@ import os
 
 def arbol(path , nivel= 0 ):
 
-    # Contadores para poder tabular segun directorio
-    total_archivos = 0
-    total_directorios = 0
-
     # Los archivos ordenados alfabeticamente
     files = sorted(
         (os.path.join(path, filename) for filename in os.listdir(path)),
@@ -23,13 +19,9 @@ def arbol(path , nivel= 0 ):
 
     for item in files:
         if os.path.isdir(item):
-            total_directorios += 1
-            #print nivel_tab +  ('\x1b[6;30;42m %s \x1b[0m') % os.path.basename(item) + "- " + item + "Nivel " + str(nivel)
             print nivel_tab +  ('\x1b[6;30;42m %s \x1b[0m') % os.path.basename(item)
-            #print "\t" +  "Total Archivos %d total Dirs %d" % (total_archivos, total_directorios)
             arbol(item, nivel + 1)
         else:
-            total_archivos += 1
             print nivel_tab + ""+ os.path.basename(item)
 
 
