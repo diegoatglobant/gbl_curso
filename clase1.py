b2903b3952207839118e2768769553dbffe11900
dilinayo <diego.x.linayo.-nd@disney.com>
2016-12-27 11:17:20 -0300

    Refactorizacion de codigo

--- clase1.py
+++ clase1.py
@@ -2,53 +2,53 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Diego Linayo'
 
-from practicas.clase1 import conjuntos
+from practicas.clase1 import EmuladorSets,Matrices
 
 
 if __name__ == "__main__":
     # Agregar un elemento al Set.
 
-    e1 = conjuntos.EmuladorSets()
+    e1 = EmuladorSets()
     e1.insertar_item("a")
     e1.insertar_item("b")
     e1.insertar_item("c")
     e1.insertar_item("d")
 
-    e2 = conjuntos.EmuladorSets()
+    e2 = EmuladorSets()
     e2.insertar_item("d")
     e2.insertar_item("e")
     e2.insertar_item("f")
     e2.insertar_item("g")
 
 
-    e3 = conjuntos.EmuladorSets(['a','b'])
+    e3 = EmuladorSets(['a','b'])
 
     # Diferencia entre un Set y otro Set.
-    diff = conjuntos.EmuladorSets.diferencias_entre_sets(e1,e2)
+    diff = EmuladorSets.diferencias_entre_sets(e1,e2)
     print "Diff: " + repr(diff)
 
 
     # Intersección entre un Set y otro Set.
-    interseccion = conjuntos.EmuladorSets.intersecciones_entre_sets(e1, e2)
+    interseccion = EmuladorSets.intersecciones_entre_sets(e1, e2)
     print "Interseccion: " + repr(interseccion)
 
 
 
     # Un método que determine si un Set está incluído en otro Set.
-    validacion = conjuntos.EmuladorSets.comparar_inclusion_entre_sets(e1, e1)
+    validacion = EmuladorSets.comparar_inclusion_entre_sets(e1, e1)
     print "Esta un set en otro? " +  repr(validacion)
 
-    validacion = conjuntos.EmuladorSets.comparar_inclusion_entre_sets(e2, e3)
+    validacion = EmuladorSets.comparar_inclusion_entre_sets(e2, e3)
     print "Esta un set en otro? " + repr(validacion)
 
 
 
     # Diferencia simétrica entre un Set y otro Set.
-    simetria = conjuntos.EmuladorSets.diferencia_simetrica(e1, e2)
+    simetria = EmuladorSets.diferencia_simetrica(e1, e2)
     print "Diff Simetrica: " + repr(simetria)
 
     #Producto cartesiano entre un Set y otro Set.
-    producto_cartesiano = conjuntos.EmuladorSets.producto_cartesiano(e1,e2)
+    producto_cartesiano = EmuladorSets.producto_cartesiano(e1,e2)
     print "Producto Cartesiano: " + str(list(producto_cartesiano))
 
     # Potencia de Conjuntos
--- practicas/clase1/__init__.py
+++ practicas/clase1/__init__.py
@@ -1,2 +1,4 @@
 # Inicializador de practicas.clase1
 # Aca podriamos definir constantes o variables del paquete.
+from conjuntos import EmuladorSets
+from matrices import Matrices
