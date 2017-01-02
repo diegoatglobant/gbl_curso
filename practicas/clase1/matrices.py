de85a2ac1c1fa6014f73c2b65d3182e7f205d086
dilinayo <diego.x.linayo.-nd@disney.com>
2017-01-02 14:14:02 -0300

    sincronizacion con branch dev

--- practicas/clase1/matrices.py
+++ practicas/clase1/matrices.py
@@ -23,7 +23,7 @@ class Matrices:
     def __str__(self):
         r = ''
         for filas in self.getItems():
-            r += str(map( lambda x: '{:4d}'.format(x),  filas  ) ) + "\n"
+            r += str(map( lambda x: '{:4d}'.format(x)  if isinstance(x, int) else str(x).ljust(4) ,  filas  ) ) + "\n"
         return str(r)
 
 
@@ -52,13 +52,24 @@ class Matrices:
     def obtener_columnas(self, matriz, indice):
         return [zip(*matriz.getItems())[i]for i in indice]
 
+    def filtro_matriz(self, matriz, indice_columna, cantidad_columnas):
+        tmp_matriz = Matrices()
+        for filas in matriz.getItems():
+            tpm_list= []
+            for id, elementos in enumerate(filas):
+                if id >= indice_columna and id <= indice_columna+cantidad_columnas:
+                    tpm_list.append(elementos)
+
+            tmp_matriz.insertar_item(tpm_list)
+        return tmp_matriz
 
 if __name__ == "__main__":
-    m1 = Matrices([10,20,30] , [1,1,1], [1,1,1])
+    m1 = Matrices([1,20,30, 1] , [1,20,30, 1], [1,30,40, 1], [1,30,40, 1], [1,40,50, 1], [1,40,50, 1], [1,60,70, 1])
+    m1_b = Matrices([1,20,30, 1] , [1,20,30, 1], [1,30,40, 1], [1,30,40, 1], [1,40,50, 1], [1,40,50, 1], [1,60,70, 1])
     m2 = Matrices([4,3,4], [4,4,4], [1,1,1])
 
     m3 = Matrices()
-    m3.suma(m1 , m2)
+    m3.suma(m1 , m1_b)
     print repr(m3)
 
     producto_escalar = m3.producto_escalar(2)
@@ -70,13 +81,16 @@ if __name__ == "__main__":
     transpuesta = m1.matriz_transpuesta()
     print repr(transpuesta)
 
-    #print "Imprimir en forma de Matriz"
+    # #print "Imprimir en forma de Matriz"
     print m1
-
 
     filas = m1.obtener_filas(m1, [0])
     print "filas: " + str(filas)
 
-
-    cols = m1.obtener_columnas(m1, [1, 0]) 
+    cols = m1.obtener_columnas(m1, [1, 0])
     print "columnas: " + str(cols)
+
+    parte_de_matriz = m1.filtro_matriz(m1, 0, 1)
+    print parte_de_matriz
+
+
